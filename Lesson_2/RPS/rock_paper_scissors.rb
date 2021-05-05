a2220646e9cb82bcdfad5c4810b82a753d442771
Leslie Hawk <lesliehawk@gmail.com>
2021-05-05 11:27:43 -0700

    update files

--- Lesson_2/RPS/rock_paper_scissors.rb
+++ Lesson_2/RPS/rock_paper_scissors.rb
@@ -1,15 +1,20 @@
-# require "yaml"
-# MSG = YAML.load_file("mortgage_calc_messages.yml")
-
-VALID_CHOICES = {'rock' => 'r', 'paper' => 'p', 'scissors' => 'sc', 'lizard' => 'l', 'spock' => 'sp'}
-WINNING_COMBOS = {
-  'rock'     => ['lizard',  'scissors'],
-  'paper'    => ['rock',    'spock'],
-  'scissors' => ['lizard',  'paper'],
-  'lizard'   => ['paper',   'spock'],
-  'spock'    => ['rock',    'scissors']
+VALID_CHOICES = {
+  'rock' => 'r',
+  'paper' => 'p',
+  'scissors' => 'sc',
+  'lizard' => 'l',
+  'spock' => 'sp'
 }
-SCOREBOARD = {player: 0, computer: 0}
+
+WINNING_COMBOS = {
+  'rock' => ['lizard', 'scissors'],
+  'paper' => ['rock', 'spock'],
+  'scissors' => ['lizard', 'paper'],
+  'lizard' => ['paper', 'spock'],
+  'spock' => ['rock', 'scissors']
+}
+
+POINTS_TO_WIN = 5
 
 def clear_screen
   system('clear')
@@ -20,8 +25,34 @@ def prompt(message)
 end
 
 def greeting
-  prompt("Welcome to Rock, Paper, Scissors, Lizard, Spock! <<")
-  prompt("Get 5 Point to Win the Match!")
+  puts "*** Welcome to Rock, Paper, Scissors, Lizard, Spock! ***"
+  puts
+end
+
+def instructions
+  prompt("Here are the rules:")
+  puts
+  puts <<-MSG
+    Scissors cuts Paper,          Scissors decapitates Lizard,
+    Paper covers Rock,            Lizard eats Paper,
+    Rock crushes Lizard,          Paper disproves Spock,
+    Lizard poisons Spock,         Spock vaporizes Rock,
+    Spock smashes Scissors,       Rock crushes Scissors.
+    MSG
+  puts
+  prompt("Get #{POINTS_TO_WIN} Points to Win the Match!")
+end
+
+def ready?
+  puts
+  prompt("Ready to shoot? (Y/N)")
+  input = ''
+  loop do
+    input = gets.strip.chomp.downcase
+    break if ['yes', 'y', 'no', 'n'].include?(input)
+    prompt("Invalid answer. Please enter Y or N.")
+  end
+  input[0] == 'y'
 end
 
 def get_choice
@@ -29,7 +60,7 @@ def get_choice
   prompt("Choose one: #{VALID_CHOICES.keys.join(', ')}")
   loop do
     prompt("Enter your choice: #{VALID_CHOICES.values.join(', ')}")
-    choice = gets.chomp.downcase
+    choice = gets.strip.chomp.downcase
     choice = VALID_CHOICES.key(choice)
     VALID_CHOICES.key?(choice) ? break : prompt("That's not a valid choice.")
   end
@@ -38,56 +69,58 @@ end
 
 def display_choices(choice, computer_choice)
   prompt("You chose:      #{choice.capitalize}")
+  # sleep(0.8)
   prompt("Computer chose: #{computer_choice.capitalize}")
+  # sleep(0.7)
 end
 
 def win?(first, second)
   WINNING_COMBOS[first].include?(second)
 end
 
-def display_results(player, computer)
+def display_results(player, computer, number)
+  puts
   if win?(player, computer)
-    prompt('YOU WON!')
+    prompt("YOU win round #{number}!")
     :player
   elsif win?(computer, player)
-    prompt("COMPUTER WON!")
+    prompt("COMPUTER wins round #{number}!")
     :computer
   else
-    prompt("It's a tie!")
+    prompt("Round #{number} is a tie!")
   end
 end
 
-def increment_score(result)
-  result == nil ? SCOREBOARD : SCOREBOARD[result] += 1
+def increment_score(result, scorebrd)
+  result.nil? ? scorebrd : scorebrd[result] += 1
 end
 
-def display_scoreboard
+def display_scoreboard(scorebrd)
   puts
-  p "************SCOREBOARD*************"
-  puts
-  prompt("Player:   #{SCOREBOARD[:player]}")
-  prompt("Computer: #{SCOREBOARD[:computer]}")
-  puts
-  p "***********************************"
+  puts " *********SCOREBOARD*********"
+  puts " |                          |"
+  puts " |       Player:   #{scorebrd[:player]}        |"
+  puts " |       Computer: #{scorebrd[:computer]}        |"
+  puts " |                          |"
+  puts " ****************************"
   puts
 end
 
-def display_match_winner
-  p "-----------------------------------"
-  prompt("MATCH POINTS:")
-  prompt("#{SCOREBOARD.key(5).to_s.capitalize} won!")
-  p "-----------------------------------"
+def display_match_winner(scorebrd)
+  puts
+  puts "*** #{scorebrd.key(POINTS_TO_WIN).to_s.upcase} wins the match!!! ***"
+  puts
 end
 
-def match_over?
-  SCOREBOARD.values.include?(5)
+def match_over?(scorebrd)
+  scorebrd.values.include?(POINTS_TO_WIN)
 end
 
 def play_again?
   prompt("Want to play another match?")
   input = ''
   loop do
-    input = gets.chomp.downcase
+    input = gets.strip.chomp.downcase
     break if ['yes', 'y', 'no', 'n'].include?(input)
     prompt("Invalid answer. Please put yes or no (Y/N).")
   end
@@ -95,41 +128,45 @@ def play_again?
 end
 
 def send_off
+  clear_screen
   prompt("Thank you for playing Rock, Paper, Scissors, Lizard, Spock!")
   prompt("Good bye!")
+  puts
 end
-
 
 clear_screen
 
 greeting
 
-loop do
-  clear_screen
+instructions
 
+loop do
+  break unless ready?
+
+  clear_screen
+  scoreboard = { player: 0, computer: 0 }
+  round = 0
   loop do
     choice = get_choice
 
-    clear_screen
-
     computer_choice = VALID_CHOICES.keys.sample
+
+    clear_screen
 
     display_choices(choice, computer_choice)
 
-    result = display_results(choice, computer_choice)
+    round += 1
 
-    increment_score(result)
+    result = display_results(choice, computer_choice, round)
 
-    display_scoreboard
+    increment_score(result, scoreboard)
 
-    break if match_over?
+    display_scoreboard(scoreboard)
+
+    break if match_over?(scoreboard)
   end
 
-  clear_screen
-
-  display_scoreboard
-
-  display_match_winner
+  display_match_winner(scoreboard)
 
   break unless play_again?
 end
